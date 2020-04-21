f07807a527f7a53d0ca61404dea6a7d1a4e92bbe
LM Train <layconmm@msn.com>
2020-04-21 14:19:53 -0500

    Add statement to show dialog message box

---
+++ MyTestingEnvironment/src/lm/programming/Main.java
@@ -0,0 +1,9 @@
+package lm.programming;
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    JOptionPane.showMessageDialog(null, "Welcome To My Java Testing Environment");
+    }
+}
