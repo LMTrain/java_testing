bf830732118e3db62aeb511fc71459902e44c4df
LMTrain <layconmm@msn.com>
2020-04-21 18:46:41 -0500

    Add Expression

--- MyTestingEnvironment/src/lm/programming/Main.java
+++ MyTestingEnvironment/src/lm/programming/Main.java
@@ -4,6 +4,22 @@ import javax.swing.JOptionPane;
 public class Main {
 
     public static void main(String[] args) {
-	    JOptionPane.showMessageDialog(null, "Welcome To My Java Testing Environment");
+        JOptionPane.showMessageDialog(null, "Welcome To My Java Testing Environment", "Autor: Laycon Muriziq", (JOptionPane.INFORMATION_MESSAGE));
+        
+        //Expression - A mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        int highScore = 50; // An expression without the int
+        if (highScore == 50) {
+            //Control statement
+            System.out.println("This is an expression");
+        }
+
+        // In the following code below, write down what parts of the code are expressiona.
+        int score = 100;
+        if (score > 90) {
+            System.out.println("You got the highest score");
+            score = 0;
+        }
+        
     }
 }
