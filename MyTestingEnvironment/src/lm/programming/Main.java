0ad64bdf1b3bda65087975fe24671731806ed7e5
LMTrain <layconmm@msn.com>
2020-04-28 15:32:51 -0500

    Add more Method Overloading

--- MyTestingEnvironment/src/lm/programming/Main.java
+++ MyTestingEnvironment/src/lm/programming/Main.java
@@ -11,6 +11,7 @@ public class Main {
         System.out.println("New score is " + newScore);
         calculateScore(75);
         calculateScore();
+        calcFeetAndInchesToCentimeters(6, 0);
 
        double miles = SpeedConverter.toMilesPerHour(10.5);
        System.out.println("Miles = " + miles);
@@ -144,6 +145,17 @@ public class Main {
     public static int calculateScore() {
         System.out.println("No Player name, no player score.");
         return 0;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        if((feet <0) || ((inches <0) && (inches >12))) {
+            System.out.println("Invalid feet or inches parameters");
+            return -1;
+        }
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + " feet, " + inches + " inches = " + centimeters + " cm");
+        return centimeters;
     }
     
 }
