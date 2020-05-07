636cf580f463f58eaccf11acd430611e1e4c45e6
LM Train <layconmm@msn.com>
2020-05-06 22:43:13 -0500

    Code for While do loop satement

--- MyTestingEnvironment/src/lm/WhileDoLoopStatement.java
+++ MyTestingEnvironment/src/lm/WhileDoLoopStatement.java
@@ -20,5 +20,23 @@ public class WhileDoLoopStatement {
                 System.out.println("Count value is " + count);
                 count++;
             } while (count != 6);
+    
+            int number = 4;
+            int finishNumber = 20;
+    
+            while (number <= finishNumber) {
+                number++;
+                if(!isEvenNumber(number)) {
+                    continue;
+                }
+                System.out.println("Even number " + number);
+            }
+        }
+        public static boolean isEvenNumber(int number) {
+            if((number % 2) == 0) {
+                return true;
+            }else {
+                return false;
+            }
         }
 }
